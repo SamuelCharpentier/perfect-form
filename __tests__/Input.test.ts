ebdfb739ee18a2054fee2fd2f8fb1fb1399ee1e3
Samuel Charpentier <scharpentier0@gmail.com>
2021-06-06 13:19:59 -0400

    adding hinting to number inputs and testing it

--- __tests__/Input.test.ts
+++ __tests__/Input.test.ts
@@ -10,9 +10,6 @@ describe('Input', () => {
 		expect(txtInpt.id.length).toBeGreaterThanOrEqual(1);
 	});
 });
-describe('Maskable Input', () => {
-	it.todo('Has mask');
-});
 
 describe('TextualInput', () => {
 	it('Validate a string value', () => {
@@ -91,6 +88,10 @@ describe('TextualInput', () => {
 		txtInpt.value = 'George';
 		expect(txtInpt.value).toBe('George');
 	});
+	it('Can be masked', () => {
+		let txtInpt = new TextualInput({ ...inputBase, mask: '000 $' });
+		expect(txtInpt.mask).toBe('000 $');
+	});
 });
 
 describe('NumericalInputs', () => {
@@ -100,28 +101,144 @@ describe('NumericalInputs', () => {
 		expect(nbrInpt.isValid).toBe(true);
 		nbrInpt.value = '0025846';
 		expect(nbrInpt.isValid).toBe(true);
-		nbrInpt.value = 66;
-		expect(nbrInpt.isValid).toBe(false);
+
 		nbrInpt.value = '';
 		expect(nbrInpt.isValid).toBe(true);
 	});
-	it('Invalidates non numerical values', () => {
+
+	it('Converts native numbers into strings', () => {
 		let nbrInpt = new NumericalInput({ ...inputBase });
-		nbrInpt.value = 'This is not a number';
-		expect(nbrInpt.isValid).toBe(false);
-		nbrInpt.value = { value: '0' };
-		expect(nbrInpt.isValid).toBe(false);
-		nbrInpt.value = [6, 3, 13];
-		expect(nbrInpt.isValid).toBe(false);
-		nbrInpt.value = undefined;
-		expect(nbrInpt.isValid).toBe(false);
-		nbrInpt.value = null;
-		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = 66;
+		expect(nbrInpt.value).toBe('66');
+		expect(nbrInpt.isValid).toBe(true);
 	});
-	it('Converts floats to intergers', () => {
+
+	it('Converts invalid values into empty strings', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true });
+		nbrInpt.value = 'This is not a number';
+		expect(nbrInpt.value).toBe('');
+		nbrInpt.value = { value: '0' };
+		expect(nbrInpt.value).toBe('');
+		nbrInpt.value = [6, 3, 13];
+		expect(nbrInpt.value).toBe('');
+		nbrInpt.value = undefined;
+		expect(nbrInpt.value).toBe('');
+		nbrInpt.value = null;
+		expect(nbrInpt.value).toBe('');
+	});
+	it('Invalidates empty value when required', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true });
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '65px';
+		expect(nbrInpt.value).toBe('65');
+		expect(nbrInpt.isValid).toBe(true);
+	});
+	it('Handles integers correctly', () => {
 		let nbrInpt = new NumericalInput({ ...inputBase, type: 'integers' });
 		nbrInpt.value = '55.2';
 		expect(nbrInpt.valueAsNumber).toBe(55);
-		expect(nbrInpt.value).toBe(55);
+		expect(nbrInpt.value).toBe('55');
+		nbrInpt.value = '33';
+		expect(nbrInpt.valueAsNumber).toBe(33);
+		expect(nbrInpt.value).toBe('33');
+	});
+
+	it('Handles floats correctly', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, type: 'floats' });
+		nbrInpt.value = '55.2';
+		expect(nbrInpt.valueAsNumber).toBe(55.2);
+		expect(nbrInpt.value).toBe('55.2');
+		nbrInpt.value = '33';
+		expect(nbrInpt.valueAsNumber).toBe(33);
+		expect(nbrInpt.value).toBe('33');
+	});
+
+	it('Enforce a minimum numerical value', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true, minVal: 10 });
+		nbrInpt.value = '-10';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '0';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '5';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '10';
+		expect(nbrInpt.isValid).toBe(true);
+	});
+
+	it('Enforce a maximum numerical value', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true, maxVal: 10 });
+		nbrInpt.value = '15';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '10';
+		expect(nbrInpt.isValid).toBe(true);
+		nbrInpt.value = '0';
+		expect(nbrInpt.isValid).toBe(true);
+		nbrInpt.value = '-100';
+		expect(nbrInpt.isValid).toBe(true);
+	});
+	it('Enforce incremental steps to value', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true, incrementStep: 2 });
+		nbrInpt.value = '3';
+		expect(nbrInpt.isValid).toBe(false);
+		nbrInpt.value = '0';
+		expect(nbrInpt.isValid).toBe(true);
+		nbrInpt.value = '2';
+		expect(nbrInpt.isValid).toBe(true);
+		nbrInpt.value = '-122';
+		expect(nbrInpt.isValid).toBe(true);
+	});
+	it('Handles input hinting properly', () => {
+		let nbrInpt = new NumericalInput({ ...inputBase, required: true, incrementStep: 2, minVal: 5, maxVal: 20 });
+
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('this field is required');
+		expect(nbrInpt.hints).toContain('this field has to be between 5 and 20');
+		nbrInpt.value = '3';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('this field has to be more than 5');
+		expect(nbrInpt.hints).toContain('this field has to be in increments of 2');
+		nbrInpt.value = '25';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('this field has to be less than 20');
+		expect(nbrInpt.hints).toContain('this field has to be in increments of 2');
+		nbrInpt.value = '10';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toStrictEqual([]);
+	});
+	it('Allows to translate hinting', () => {
+		let myTranslation = {
+			thisField: 'ce champ',
+			isRequired: 'est requis',
+			hasToBe: 'doit être',
+			lessThan: 'moins de',
+			moreThan: 'plus de',
+			between: 'entre',
+			and: 'et',
+			inIncrementsOf: 'en incréments de',
+		};
+		let nbrInpt = new NumericalInput({
+			...inputBase,
+			required: true,
+			incrementStep: 2,
+			minVal: 5,
+			maxVal: 20,
+			hintConfig: { translation: myTranslation },
+		});
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('ce champ est requis');
+		expect(nbrInpt.hints).toContain('ce champ doit être entre 5 et 20');
+		nbrInpt.value = '3';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('ce champ doit être plus de 5');
+		expect(nbrInpt.hints).toContain('ce champ doit être en incréments de 2');
+		nbrInpt.value = '25';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toContain('ce champ doit être moins de 20');
+		expect(nbrInpt.hints).toContain('ce champ doit être en incréments de 2');
+		nbrInpt.value = '10';
+		nbrInpt.updateHint();
+		expect(nbrInpt.hints).toStrictEqual([]);
 	});
 });
--- src/NumericalInput.ts
+++ src/NumericalInput.ts
@@ -1,16 +1,57 @@
-import { MaskableInputConstructor, MaskableInput } from './Input';
+import { InputConstructor, Input } from './Input';
 import type { InputOption } from './Option';
 
-interface NumberInputConstructor extends MaskableInputConstructor {
+const numberInputDefaultHintTranslation = {
+	thisField: 'this field',
+	isRequired: 'is required',
+	hasToBe: 'has to be',
+	lessThan: 'less than',
+	moreThan: 'more than',
+	between: 'between',
+	and: 'and',
+	inIncrementsOf: 'in increments of',
+};
+const numberInputDefaultHintToggle = {
+	required: true,
+	min: true,
+	max: true,
+	increment: true,
+};
+
+const numberInputDefaultHintConfig = {
+	translation: numberInputDefaultHintTranslation,
+	toggle: numberInputDefaultHintToggle,
+};
+interface HintConfig {
+	translation?: {
+		thisField: string;
+		isRequired: string;
+		hasToBe: string;
+		lessThan: string;
+		moreThan: string;
+		between: string;
+		and: string;
+		inIncrementsOf: string;
+	};
+	toggle?: {
+		required?: boolean;
+		min?: boolean;
+		max?: boolean;
+		increment?: boolean;
+	};
+}
+
+interface NumberInputConstructor extends InputConstructor {
 	value?: string;
 	type?: 'integers' | 'floats';
 	minVal?: number;
 	maxVal?: number;
 	autocomplete?: boolean | string;
 	incrementStep?: number;
+	hintConfig?: HintConfig;
 }
 
-export class NumericalInput extends MaskableInput {
+export class NumericalInput extends Input {
 	public autocomplete: boolean | string;
 	public defaultValue: string;
 	public type: 'integers' | 'floats';
@@ -18,14 +59,41 @@ export class NumericalInput extends MaskableInput {
 	public maxVal?: number;
 	public incrementStep?: number;
 	public optionList?: InputOption[];
-	public hints?: string[];
+	public hints: string[] = [];
 	public customValidation: ((value: string) => boolean)[] = [];
+	public hintConfig: {
+		translation: {
+			thisField: string;
+			isRequired: string;
+			hasToBe: string;
+			lessThan: string;
+			moreThan: string;
+			between: string;
+			and: string;
+			inIncrementsOf: string;
+		};
+		toggle: {
+			required: boolean;
+			min: boolean;
+			max: boolean;
+			increment: boolean;
+		};
+	};
 	private _value: string = '';
 
 	constructor(constructorObject: NumberInputConstructor) {
 		super(constructorObject);
-		const { value, type, minVal, maxVal, autocomplete, incrementStep, defaultValue, useValueAsDefaultValue } =
-			constructorObject;
+		const {
+			value,
+			type,
+			minVal,
+			maxVal,
+			autocomplete,
+			incrementStep,
+			defaultValue,
+			useValueAsDefaultValue,
+			hintConfig,
+		} = constructorObject;
 		this.value = value !== undefined ? value : '';
 		this.defaultValue = defaultValue !== undefined ? defaultValue : useValueAsDefaultValue ? value : '';
 		this.type = type ? type : 'floats';
@@ -33,26 +101,124 @@ export class NumericalInput extends MaskableInput {
 		this.maxVal = maxVal;
 		this.autocomplete = autocomplete !== undefined ? autocomplete : true; // true could be changed for a default value for all form fields...
 		this.incrementStep = incrementStep;
+
+		this.hintConfig = {
+			translation: { ...numberInputDefaultHintConfig.translation, ...hintConfig?.translation },
+			toggle: { ...numberInputDefaultHintConfig.toggle, ...hintConfig?.toggle },
+		};
 	}
-	set value(value: string) {
-		value = value !== undefined && typeof value === 'string' ? value : '';
-		let numberValue = this.convertToNumber(value, this.type);
-		if (value !== '' && numberValue) {
+
+	set value(value: string | number) {
+		if (typeof value === 'number') value = value?.toString();
+		if (value === '' || typeof value !== 'string') {
+			this._value = '';
+			return;
 		}
+		let numberValue = this.convertToNumber(value, this.type);
+		value = numberValue !== undefined ? numberValue.toString() : '';
 		this._value = value;
 	}
+
 	get value() {
 		return this._value;
 	}
-	updateHint() {}
-	get isValid() {
-		return this.validate(this.value);
+	private get needsToShowRequiredHint() {
+		return this.required && this.hintConfig.toggle.required;
 	}
+	private get needsToShowMinHint() {
+		return this.minVal !== undefined && this.hintConfig.toggle.min;
+	}
+	private get needsToShowMaxHint() {
+		return this.minVal !== undefined && this.hintConfig.toggle.min;
+	}
+	private get needsToShowIncrementHint() {
+		return this.incrementStep !== undefined && this.hintConfig.toggle.increment;
+	}
+	private addToHints(message: string) {
+		if (!this.hints.includes(message)) this.hints.push(message);
+	}
+	private removeFromHints(message: string) {
+		if (this.hints.includes(message)) this.hints.splice(this.hints.indexOf(message), 1);
+	}
+	updateHint() {
+		const msg = this.hintConfig.translation;
+		if (this.needsToShowRequiredHint) {
+			let hintMessageRequired = `${msg.thisField} ${msg.isRequired}`;
+			if (!this.isValid) {
+				this.addToHints(hintMessageRequired);
+			} else {
+				this.removeFromHints(hintMessageRequired);
+			}
+		}
+		if (this.needsToShowMinHint || this.needsToShowMaxHint) {
+			let hintMessageMinMax = `${msg.thisField} ${msg.hasToBe} ${msg.between} ${this.minVal} ${msg.and} ${this.maxVal}`;
+			let hintMessageMin = `${msg.thisField} ${msg.hasToBe} ${msg.moreThan} ${this.minVal}`;
+			let hintMessageMax = `${msg.thisField} ${msg.hasToBe} ${msg.lessThan} ${this.maxVal}`;
+			if (this.valueAsNumber === undefined) {
+				if (this.needsToShowMinHint && this.needsToShowMinHint) {
+					this.addToHints(hintMessageMinMax);
+				} else {
+					this.removeFromHints(hintMessageMinMax);
+				}
+				if (this.needsToShowMinHint && !this.needsToShowMaxHint) {
+					this.addToHints(hintMessageMin);
+				} else {
+					this.removeFromHints(hintMessageMin);
+				}
+				if (this.needsToShowMaxHint && !this.needsToShowMinHint) {
+					this.addToHints(hintMessageMax);
+				} else {
+					this.removeFromHints(hintMessageMax);
+				}
+			} else {
+				if (this.needsToShowMinHint && this.needsToShowMaxHint) {
+					this.removeFromHints(hintMessageMinMax);
+				}
+				if (this.needsToShowMinHint && this.minVal && this.valueAsNumber < this.minVal) {
+					this.addToHints(hintMessageMin);
+				} else {
+					this.removeFromHints(hintMessageMin);
+				}
+				if (this.needsToShowMaxHint && this.maxVal && this.valueAsNumber > this.maxVal) {
+					this.addToHints(hintMessageMax);
+				} else {
+					this.removeFromHints(hintMessageMax);
+				}
+			}
+			if (this.needsToShowIncrementHint) {
+				let hintMessageIncrement = `${msg.thisField} ${msg.hasToBe} ${msg.inIncrementsOf} ${this.incrementStep}`;
+				if (
+					this.valueAsNumber !== undefined &&
+					this.incrementStep !== undefined &&
+					this.valueAsNumber % this.incrementStep !== 0
+				) {
+					this.addToHints(hintMessageIncrement);
+				} else {
+					this.removeFromHints(hintMessageIncrement);
+				}
+			}
+
+			// if (this?.valueAsNumber < this?.minVal || this?.valueAsNumber > this?.maxVal) {
+			// 	if (tgl.min && tgl.max && this.minVal !== undefined && this.maxVal !== undefined) {
+			// 		this.addToHints(
+			// 			`${msg.thisField} ${msg.hasToBe} ${msg.between} ${this.minVal} ${msg.and} ${this.maxVal}`,
+			// 		);
+			// 	}
+			// 	if (tgl.min && this.valueAsNumber < this.minVal) {
+			// 		this.addToHints(`${msg.thisField} ${msg.hasToBe} ${msg.above} ${this.minVal}`);
+			// 	}
+			// }
+		}
+	}
+
+	get isValid() {
+		return this.validate(this._value);
+	}
+
 	private validate(value: string) {
 		if (!this.required && value === '') return true;
 		const numberValue = this.convertToNumber(value, this.type);
 		if (numberValue === undefined) return false;
-		if (isNaN(numberValue)) return false;
 		if (this.minVal && numberValue < this.minVal) return false;
 		if (this.maxVal && numberValue > this.maxVal) return false;
 		if (this.type === 'integers' && !Number.isInteger(numberValue)) return false;
@@ -62,13 +228,14 @@ export class NumericalInput extends MaskableInput {
 		});
 		return true;
 	}
+
 	get valueAsNumber(): number | undefined {
-		return this.convertToNumber(this.value, this.type);
+		return this.convertToNumber(this._value, this.type);
 	}
+
 	private convertToNumber(value: string, type: 'floats' | 'integers') {
 		if (typeof value !== 'string') return undefined;
-		if (type === 'floats') return parseFloat(this.value);
-		if (type === 'integers') return parseInt(this.value, 10);
-		return undefined;
+		let convertedValue = type === 'floats' ? parseFloat(value) : parseInt(value, 10);
+		return isNaN(convertedValue) ? undefined : convertedValue;
 	}
 }
